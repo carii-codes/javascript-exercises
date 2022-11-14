48c3e90f85162715dd1ad62fbe63231103037351
Carina C <cardozo1747@gmail.com>
2022-11-13 18:48:03 -0600

    test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return ""; 
+    } 
+    else if (num === 1) {
+        return str;
+    }
+    else {
+        return str + repeatString(str, num - 1);
+    }
 };
-
+  repeatString("hey", 3)  
+  
 // Do not edit below this line
 module.exports = repeatString;
